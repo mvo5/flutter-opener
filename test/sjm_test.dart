f447b6954a8bd22586fbb9e6c088f5dac7d0c748
Michael Vogt <michael.vogt@gmail.com>
2024-07-24 18:24:41 +0200

    test: run `dart format`

--- test/sjm_test.dart
+++ test/sjm_test.dart
@@ -3,23 +3,25 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:muopener/sjm.dart';
 
 void main() {
-    test('SignedJsonMessage signed smoke', () {
-	var sjm = SignedJsonMessage("key", "nonce");
-	sjm.setPayload({"foo": "bar"});
-	expect(sjm.nonce, equals("nonce"));
-    });
+  test('SignedJsonMessage signed smoke', () {
+    var sjm = SignedJsonMessage("key", "nonce");
+    sjm.setPayload({"foo": "bar"});
+    expect(sjm.nonce, equals("nonce"));
+  });
 
-    test('SignedJsonMessage.from_string', () {
-	var s = "eyJ2ZXIiOiAiMSIsICJhbGciOiAiSFMyNTYiLCAibm9uY2UiOiAibm9uY2UifQ==.e30=.iCQB3KZ4Kd9wnDrhYtTF/TBX3iJLFgApBfX+wMGw5hY=";
-	var sjm = SignedJsonMessage.fromString(s, "key", "nonce");
-	// XXX: test more
-	expect(sjm.payload, equals({}));
-    });
+  test('SignedJsonMessage.from_string', () {
+    var s =
+        "eyJ2ZXIiOiAiMSIsICJhbGciOiAiSFMyNTYiLCAibm9uY2UiOiAibm9uY2UifQ==.e30=.iCQB3KZ4Kd9wnDrhYtTF/TBX3iJLFgApBfX+wMGw5hY=";
+    var sjm = SignedJsonMessage.fromString(s, "key", "nonce");
+    // XXX: test more
+    expect(sjm.payload, equals({}));
+  });
 
-    test('SignedJsonMessage.from_string expected nonce is optional', () {
-	var s = "eyJ2ZXIiOiAiMSIsICJhbGciOiAiSFMyNTYiLCAibm9uY2UiOiAibm9uY2UifQ==.e30=.iCQB3KZ4Kd9wnDrhYtTF/TBX3iJLFgApBfX+wMGw5hY=";
-	var sjm = SignedJsonMessage.fromString(s, "key", "");
-	// XXX: test more
-	expect(sjm.payload, equals({}));
-    });
+  test('SignedJsonMessage.from_string expected nonce is optional', () {
+    var s =
+        "eyJ2ZXIiOiAiMSIsICJhbGciOiAiSFMyNTYiLCAibm9uY2UiOiAibm9uY2UifQ==.e30=.iCQB3KZ4Kd9wnDrhYtTF/TBX3iJLFgApBfX+wMGw5hY=";
+    var sjm = SignedJsonMessage.fromString(s, "key", "");
+    // XXX: test more
+    expect(sjm.payload, equals({}));
+  });
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -19,46 +19,48 @@ import 'package:muopener/api.dart';
 
 import 'widget_test.mocks.dart';
 
-@GenerateMocks([OpenerApi,FlutterSecureStorage])
+@GenerateMocks([OpenerApi, FlutterSecureStorage])
 void main() {
-    testWidgets('Opener main flow with valid config', (WidgetTester tester) async {
-	var storage = MockFlutterSecureStorage();
-	when(storage.read(key: "cfg")).thenAnswer(
-	    (_) async => '{"hostname": "opener", "hmac-key": "hmackey"}'
-	);
-	
-	var opener = MockOpenerApi();
-	// TODO: add test that device-info is correct
-	when(opener.init("opener", 8877, "hmackey", "unknown")).thenReturn(0);
-	when(opener.open()).thenAnswer((_) async => "Opening...");
+  testWidgets('Opener main flow with valid config',
+      (WidgetTester tester) async {
+    var storage = MockFlutterSecureStorage();
+    when(storage.read(key: "cfg")).thenAnswer(
+        (_) async => '{"hostname": "opener", "hmac-key": "hmackey"}');
 
-	// Build our app
-	var app = OpenerApp();
-	await tester.pumpWidget(app);
+    var opener = MockOpenerApi();
+    // TODO: add test that device-info is correct
+    when(opener.init("opener", 8877, "hmackey", "unknown")).thenReturn(0);
+    when(opener.open()).thenAnswer((_) async => "Opening...");
 
-	// add mocks
-	final state = tester.state(find.byType(OpenerHomePage)) as OpenerHomePageState;
-	state.opener = opener;
-	state.storage = storage;
-	// readCfg must be called again with fake storage
-	state.readCfg();
-	// XXX: why does "await tester.pumpAndSettle()" does not work here ?
-	await tester.pumpWidget(OpenerApp(),  Duration(milliseconds: 100));
+    // Build our app
+    var app = OpenerApp();
+    await tester.pumpWidget(app);
 
-	// Verify that we have a text
-	expect(find.text('Slide to open'), findsOneWidget);
-	expect(find.text('close'), findsNothing);
+    // add mocks
+    final state =
+        tester.state(find.byType(OpenerHomePage)) as OpenerHomePageState;
+    state.opener = opener;
+    state.storage = storage;
+    // readCfg must be called again with fake storage
+    state.readCfg();
+    // XXX: why does "await tester.pumpAndSettle()" does not work here ?
+    await tester.pumpWidget(OpenerApp(), Duration(milliseconds: 100));
 
-	// pretend to drag the open slider
-	await tester.drag(find.byType(SliderButton), Offset(500.0, 0.0));
-	// pumpAndSettle cannot be used because of the spinner animation
-	for (int i = 0 ; i<10; i++) {
-	    await tester.pumpWidget(app,  Duration(milliseconds: 100));
-	}
+    // Verify that we have a text
+    expect(find.text('Slide to open'), findsOneWidget);
+    expect(find.text('close'), findsNothing);
 
-	// Verify that it tries to open the door
-	expect(find.byKey(Key("label_status")), findsOneWidget);
-	var labelStatus = find.byKey(Key("label_status")).evaluate().first.widget as Text;
-	expect(labelStatus.data, "Opening...");
-    });
+    // pretend to drag the open slider
+    await tester.drag(find.byType(SliderButton), Offset(500.0, 0.0));
+    // pumpAndSettle cannot be used because of the spinner animation
+    for (int i = 0; i < 10; i++) {
+      await tester.pumpWidget(app, Duration(milliseconds: 100));
+    }
+
+    // Verify that it tries to open the door
+    expect(find.byKey(Key("label_status")), findsOneWidget);
+    var labelStatus =
+        find.byKey(Key("label_status")).evaluate().first.widget as Text;
+    expect(labelStatus.data, "Opening...");
+  });
 }
